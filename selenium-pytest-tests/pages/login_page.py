0b7e35fe6459225bc9a4ed0ec593cbc67d566b9f
dim-dimy4 <kachuk.mail@gmail.com>
2025-03-05 22:38:07 +0100

    Add files via upload

---
+++ selenium-pytest-tests/pages/login_page.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+
+class LoginPage:
+    """Page Object class for the login page on SauceDemo."""
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def open(self):
+        """Opens the login page."""
+        self.browser.get("https://www.saucedemo.com/")
+
+    def login(self, username, password):
+        """Fills in username and password, then clicks the login button."""
+        self.browser.find_element(By.ID, "user-name").send_keys(username)
+        self.browser.find_element(By.ID, "password").send_keys(password)
+        self.browser.find_element(By.ID, "login-button").click()
+
+    def get_error_message(self):
+        """Retrieves the error message if login fails."""
+        return self.browser.find_element(By.CLASS_NAME, "error-message-container").text
